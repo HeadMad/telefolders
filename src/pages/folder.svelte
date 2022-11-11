bd8e2d5ae6f3c1bd0e58000547e3d34256af7009
HeadMad <kov.ps@ya.ru>
2022-11-11 10:39:24 +0800

    1

--- src/pages/folder.svelte
+++ src/pages/folder.svelte
@@ -1,25 +1,31 @@
 <script>
-  import getFiles from "./lib/getFiles";
-  let filesPromise = getFiles(window.location.pathname);
+  import getFiles from "../lib/getFiles";
+  export let path;
+
+  // let filesPromise = getFiles(path);
 </script>
 
 {#await filesPromise}
   Ждём файлы...
 {:then files}
   <div class="image-wrap">
-    {#each files as file}
-      <!-- <pre>{JSON.stringify(file, null, ' ')}</pre> -->
-      {#if "type" in file}
-        {#if file.type === "photo"}
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/telefolders.appspot.com/o/{encodeURIComponent(
-              file.thumb
-            )}?alt=media"
-            alt=""
-          />
+    {#if !files.length}
+    <h3>В папке пока нет файлов</h3>
+    {:else}
+      {#each files as file}
+        <!-- <pre>{JSON.stringify(file, null, ' ')}</pre> -->
+        {#if "type" in file}
+          {#if file.type === "photo"}
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/telefolders.appspot.com/o/{encodeURIComponent(
+                file.thumb
+              )}?alt=media"
+              alt=""
+            />
+          {/if}
         {/if}
-      {/if}
-    {/each}
+      {/each}
+    {/if}
   </div>
 {:catch error}
   {error.message}
@@ -29,19 +35,15 @@
 
 <style>
   img {
+    margin: 10px;
     height: 180px;
     width: 180px;
     object-fit: cover;
   }
 
-  img:hover {
-    object-fit: contain;
-  }
-
   .image-wrap {
-    display: flex;
-    gap: 12px;
-    flex-wrap: wrap;
+    text-align: center;
+    /* text-justify: ; */
   }
 
   pre {
